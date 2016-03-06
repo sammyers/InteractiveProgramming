b05fde23485228710ba4c25221a755e7b9aad385
Sam Myers <sam@students.olin.edu>
2016-03-06 17:53:49 -0500

    minor tweaks

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import time
 square_width = 10 #pixels
 grid_width = 51
 pixels_wide = square_width * grid_width
-ms_per_block = 5 # screen refreshes per move
+ms_per_block = 6 # screen refreshes per move
 
 if __name__ == '__main__':
 	pygame.init()
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 		count += 1
 		if count == ms_per_block:
 			model.update_snake()
-			model.collision()
+			model.check_collision()
 			count = 0
 		view.draw()
 		time.sleep(.001)
--- models.py
+++ models.py
@@ -18,8 +18,7 @@ class GameView(object):
 				except:
 					color = 'black'
 				pygame.draw.rect(self.screen, pygame.Color(color), get_rect)
-		pygame.display.update()
-			
+		pygame.display.update()	
 
 	def coord_to_pixels(self, coords):
 		"""Take a coordinate pair on the grid and convert to pygame rectangle parameters"""
@@ -27,6 +26,7 @@ class GameView(object):
 		top = self.square_size * coords[1]
 		width, height = (self.square_size, self.square_size)
 		return (left, top, width, height)
+
 
 class GameController(object):
 	def __init__(self, model):
@@ -50,7 +50,9 @@ class GameController(object):
 					}
 		new_direction = control_dict[event.key]
 		if new_direction != opposite[self.model.snake.direction]:
-			self.model.snake.direction = new_direction 
+			self.model.snake.direction = new_direction
+
+
 class GameModel(object):
 	def __init__(self, dimensions=50):
 		self.grid = GameGrid(dimensions)
@@ -61,6 +63,32 @@ class GameModel(object):
 		self.dead = False
 		self.make_first_walls()
 		self.make_food()
+
+	def make_first_walls(self):
+		for i in range(len(self.grid.grid)):
+			first_wall = Wall(i, 0)
+			last_wall = Wall(i, len(self.grid.grid[i])-1)
+			self.grid.grid[i][0] = first_wall					# Make top and bottom lines 
+			self.grid.grid[i][-1] = last_wall
+			self.walls.extend((first_wall, last_wall))
+		for j in range(1, len(self.grid.grid[0])):				# Make side walls
+			first_wall = Wall(0, j)
+			last_wall = Wall(len(self.grid.grid[j])-1, j)
+			self.grid.grid[0][j] = first_wall
+			self.grid.grid[-1][j] = last_wall
+			self.walls.extend((first_wall, last_wall))
+
+	def make_food(self):
+		def random_point():
+			x = random.randrange(1, len(self.grid.grid) - 1)
+			y = random.randrange(1, len(self.grid.grid[x]) - 1)
+			return (x, y)
+		point = random_point()
+		while SnakeBodyPart(*point) in self.snake.get_list():
+			point = random_point()
+		new_food = Food(*point)
+		self.foods.append(new_food)
+		self.grid.grid[point[0]][point[1]] = new_food
 
 	def move_snake(self, to_x, to_y):
 		# Delete in the grid
@@ -75,15 +103,8 @@ class GameModel(object):
 		for part in self.snake.get_list():
 			self.grid.grid[part.x][part.y] = part
 
-	def make_food(self):
-		x = random.randrange(1, len(self.grid.grid) - 1)
-		y = random.randrange(1, len(self.grid.grid[x]) - 1)
-		new_food = Food(x, y)
-		self.foods.append(new_food)
-		self.grid.grid[x][y] = new_food
-
 	def update_snake(self):
-		if self.dead:
+		if self.snake.dead:
 			return
 		x = self.snake.head.data.x
 		y = self.snake.head.data.y
@@ -96,7 +117,7 @@ class GameModel(object):
 		if self.snake.direction == 'down':
 			self.move_snake(x, y + 1)
 
-	def collision(self):
+	def check_collision(self):
 		"""
 		Check if the Snake's head is hitting a wall or a food.
 		Act on those.
@@ -107,7 +128,7 @@ class GameModel(object):
 		# Check for wall collision
 		for a_wall in self.walls:
 			if a_wall.x == x and a_wall.y == y:
-				self.die()		
+				self.snake.die()		
 
 		# Check for food collision
 		for i, apple in enumerate(self.foods):
@@ -119,26 +140,8 @@ class GameModel(object):
 		# Check for snake collisions
 		for part in self.snake.get_list()[1:]:		# Don't check against the head
 			if part.x == x and part.y == y:
-				self.die()			
+				self.snake.die()		
 	
-	def die(self):
-		self.dead = True
-	
-	def make_first_walls(self):
-		for i in range(len(self.grid.grid)):
-			first_wall = Wall(i, 0)
-			last_wall = Wall(i, len(self.grid.grid[i])-1)
-			self.grid.grid[i][0] = first_wall					# Make top and bottom lines 
-			self.grid.grid[i][-1] = last_wall
-			self.walls.extend((first_wall, last_wall))
-
-		for j in range(1, len(self.grid.grid[0])):				# Make side walls
-			first_wall = Wall(0, j)
-			last_wall = Wall(len(self.grid.grid[j])-1, j)
-			self.grid.grid[0][j] = first_wall
-			self.grid.grid[-1][j] = last_wall
-			self.walls.extend((first_wall, last_wall))
-
 
 class GameGrid(object):
 	def __init__(self, dimensions):
@@ -163,12 +166,13 @@ class SnakeBodyPart(object):
 		self.index = index
 
 class Snake(LinkedList):
-	def __init__(self, position, direction=None, growth_rate=3):
+	def __init__(self, position, direction=None, growth_rate=4):
 		head = Node(SnakeBodyPart(position, position))
 		super(Snake, self).__init__(head)
 		self.direction = direction
 		self.growth_counter = 0
 		self.growth_rate = growth_rate
+		self.dead = False
 	
 	def move(self, to_x, to_y, eaten=False):
 		""" Takes a tuple of position"""
@@ -181,6 +185,9 @@ class Snake(LinkedList):
 	def grow(self):
 		self.growth_counter += self.growth_rate
 		
+	def die(self):
+		self.dead = True
+
 
 class Food(object):
 	color = 'yellow'
